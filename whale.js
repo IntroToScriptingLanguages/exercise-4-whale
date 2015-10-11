4018dc21031397f15e699518bf627cb29c3d7af5
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-10-11 14:13:25 -0500

    complete

--- whale.js
+++ whale.js
@@ -1,14 +1,18 @@
 jQuery(document).ready(function($) {
-   var num = parseInt(location.hash.substr(1));
-
-   if (isNaN(num))
-   {
-     num = 0;
-   }
-   console.log(num);
-
-   $.get('/whale/txt/'+num+'.txt', function(txt) {
-     $('a').html(txt);
-     $('a').attr('href', '#'+(num+1));
-     });
+  update();
+  window.addEventListener('hashchange', update, false)
 } );
+
+function update() { //Updates the text
+  var num = parseInt(location.hash.substr(1));
+
+  if (isNaN(num))
+  {
+    num = 0;
+  }
+
+  $.get('/whale/txt/'+num+'.txt', function(txt) {
+    $('a').html(txt);
+    $('a').attr('href', '#'+(num+1));
+    });
+}
